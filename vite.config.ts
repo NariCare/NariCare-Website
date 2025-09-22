3eebdc1e0e49d70639e2d06a3d8b18a0ac56a555
Davinder Singh <19davinder@gmail.com>
2025-09-22 09:34:49 +0530

    .

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
   css: {
     postcss: {
       plugins: [tailwind()],
